25464b08fafceb28f4ce9335ae54fef273249f36
weenta <weenta@qq.com>
2018-02-09 21:26:00 +0800

    创建数据表

--- readMe.md
+++ readMe.md
@@ -10,4 +10,17 @@ koa + mysql + react 前后分离 商城项目
 
 
 ## mysql        
+```sql
+    # 建表
+    CREATE DATABASE `o2o_db` DEFAULT CHARSET utf8;
 
+    # 引入数据
+    SOURCE 01data/04node/07o2o/sql.sql
+
+    # 修改数据库提示符PROMPT
+    PROMTP \h@\d>
+
+    \h 服务器名称
+    \d 数据库名称
+    \u 当前用户
+```
--- sql.sql
+++ sql.sql
@@ -1,4 +1,4 @@
-# 生活服务表
+# 1生活服务表A
 CREATE TABLE `o2o_category` (
     `id` INT(11) UNSIGNED  NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL DEFAULT '',
@@ -11,7 +11,7 @@ CREATE TABLE `o2o_category` (
     KEY parent_id(`parent_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;  
 
-# 城市表
+# 2城市表A
 CREATE TABLE `o2o_city` (
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL DEFAULT '',
@@ -26,7 +26,7 @@ CREATE TABLE `o2o_city` (
     KEY parent_id(`parent_id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-# 商圈表
+# 3商圈表A
 CREATE TABLE `o2o_area` (
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL DEFAULT '',
@@ -41,7 +41,7 @@ CREATE TABLE `o2o_area` (
     KEY city_id(`city_id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-# 商户表
+# 4商户表A
 CREATE TABLE `o2o_bis` (
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL DEFAULT '',
@@ -53,7 +53,7 @@ CREATE TABLE `o2o_bis` (
     `city_path` VARCHAR(50) NOT NULL DEFAULT 0,
     `bank_info` VARCHAR(50) NOT NULL DEFAULT 0,
     `money` DECIMAL(20,2) NOT NULL DEFAULT '0.00',
-    `bank_name` VARCHAR(50) NOT NULL DEFALUT '',
+    `bank_name` VARCHAR(50) NOT NULL DEFAULT '',
     `bank_user` VARCHAR(50) NOT NULL DEFAULT '',
     `faren` VARCHAR(50) NOT NULL DEFAULT '',
     `faren_tel` VARCHAR(50) NOT NULL DEFAULT '',
@@ -66,7 +66,7 @@ CREATE TABLE `o2o_bis` (
     KEY name(`name`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-# 商户表账号表
+# 5商户表账号表A
 CREATE TABLE `o2o_bis_account` (
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(50) NOT NULL DEFAULT '',
@@ -85,7 +85,7 @@ CREATE TABLE `o2o_bis_account` (
     KEY username(`username`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-# 商户门店表
+# 6商户门店表A
 CREATE TABLE `o2o_bis_location` (
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL DEFAULT '',
@@ -93,20 +93,20 @@ CREATE TABLE `o2o_bis_location` (
     `address` VARCHAR(255) NOT NULL DEFAULT '',
     `tel` VARCHAR(20) NOT NULL DEFAULT '',
     `contact` VARCHAR(20) NOT NULL DEFAULT '',
-    `xpoint` VARCHAR(20) NOT NULL DEFAULT ''
-    `ypoint` VARCHAR(20) NOT NULL DEFAULT ''
+    `xpoint` VARCHAR(20) NOT NULL DEFAULT '',
+    `ypoint` VARCHAR(20) NOT NULL DEFAULT '',
     `bis_id` INT(11) UNSIGNED NOT NULL DEFAULT 0,
-    `open_time` INT(11) INSIGNED NOT NULL DEFAULT 0,
-    `content` text NOT NULL DEFAULT '',
+    `open_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    `content` text,
     `is_main` TINYINT(1) NOT NULL DEFAULT 0,
     `api_address` VARCHAR(255) NOT NULL DEFAULT '',
     `city_id` INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `city_path` VARCHAR(50) NOT NULL DEFAULT 0,
-    `category_id` INT(11) UNSIGNED NOT NULL DEFAULT '',
-    `category_path` VARCHAR(50) NOT BULL DEFAULT '',
-    `bank_info` VARCHAR(50) NOT NULL DEFAULT 0,
+    `category_id` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    `category_path` VARCHAR(50) NOT NULL DEFAULT '',
+    `bank_info` VARCHAR(50) NOT NULL DEFAULT '',
     `money` DECIMAL(20,2) NOT NULL DEFAULT '0.00',
-    `bank_name` VARCHAR(50) NOT NULL DEFALUT '',
+    `bank_name` VARCHAR(50) NOT NULL DEFAULT '',
     `bank_user` VARCHAR(50) NOT NULL DEFAULT '',
     `faren` VARCHAR(50) NOT NULL DEFAULT '',
     `faren_tel` VARCHAR(50) NOT NULL DEFAULT '',
@@ -121,3 +121,73 @@ CREATE TABLE `o2o_bis_location` (
     KEY name(`name`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+# 7团购商品表A
+CREATE TABLE `o2o_deal`(
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL DEFAULT '',
+    `category_id` INT(11) NOT NULL DEFAULT 0,
+    `se_category_id` INT(11) NOT NULL DEFAULT 0,
+    `bis_id` INT(11) NOT NULL DEFAULT 0,
+    `location_idS` VARCHAR(100) NOT NULL DEFAULT '',
+    `image` VARCHAR(200) NOT NULL DEFAULT '',
+    `description` text NOT NULL,
+    `start_time` INT(11) NOT NULL DEFAULT 0,
+    `end_time` INT(11) NOT NULL DEFAULT 0,
+    `origin_price` DECIMAL(20,2) NOT NULL DEFAULT '0.00',
+    `current_price` DECIMAL(20,2) NOT NULL DEFAULT '0.00',
+    `city_id` INT(11) NOT NULL DEFAULT 0,
+    `buy_count` INT(11) NOT NULL DEFAULT 0,
+    `total_count` INT(11) NOT NULL DEFAULT 0,
+    `coupons_begin_time` INT(11) NOT NULL DEFAULT 0,
+    `coupons_end_time` INT(11) NOT NULL DEFAULT 0,
+    `xpoint` VARCHAR(20) NOT NULL DEFAULT '',
+    `ypoint` VARCHAR(20) NOT NULL DEFAULT '',
+    `bis_account_id` INT(11) NOT NULL DEFAULT 0,
+    `balance_price` DECIMAL(20,2) NOT NULL DEFAULT '0.00',
+    `notes` TEXT NOT NULL,
+    `faren_tel` VARCHAR(50) NOT NULL DEFAULT '',
+    `listorder` INT(8) UNSIGNED NOT NULL  DEFAULT 0,
+    `status` TINYINT(1) NOT NULL DEFAULT 0,
+    `create_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    `update_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY(`id`),
+    KEY category_id(`category_id`),
+    KEY se_category_id(`se_category_id`),
+    KEY city_id(`city_id`),
+    KEY start_time(`start_time`),
+    KEY end_time(`end_time`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+# 8用户表A
+CREATE TABLE `o2o_user`(
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL DEFAULT '',
+    `password` CHAR(32) NOT NULL DEFAULT '',
+    `code` VARCHAR(10) NOT NULL DEFAULT '',
+    `last_login_ip` VARCHAR(20) NOT NULL DEFAULT '',
+    `last_login_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    `email` VARCHAR(30) NOT NULL DEFAULT '',
+    `mobile` VARCHAR(20) NOT NULL DEFAULT '',
+    `listorder` INT(8) UNSIGNED NOT NULL  DEFAULT 0,
+    `status` TINYINT(1) NOT NULL DEFAULT 0,
+    `create_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    `update_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY username(`username`),
+    UNIQUE KEY email(`email`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+# 9推荐位表A
+CREATE TABLE `o2o_feature`(
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `type` TINYINT(1) NOT NULL DEFAULT 0,
+    `title` VARCHAR(30) NOT NULL DEFAULT '',
+    `image` VARCHAR(255) NOT NULL DEFAULT '',
+    `url` VARCHAR(255) NOT NULL DEFAULT '',
+    `description` VARCHAR(255) NOT NULL DEFAULT '',
+    `listorder` INT(8) UNSIGNED NOT NULL  DEFAULT 0,
+    `status` TINYINT(1) NOT NULL DEFAULT 0,
+    `create_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    `update_time` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY(`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
